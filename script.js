34404e93892884fee98e814ade6ef1058bb83733
Levin Baenninger <levin.baenninger@buhlergroup.com>
2024-01-05 14:42:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function toggleMenu() {
+  const icon = document.querySelector('.hamburger-icon');
+  const menu = document.querySelector('.menu-links');
+
+  icon.classList.toggle('open');
+  menu.classList.toggle('open');
+}
